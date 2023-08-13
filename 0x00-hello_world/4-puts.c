d98253ed64d9edb5c1994981656c398dea228202
Okoye Emmanuel Obiajulu <okoyeemmanuel998@gmail.com>
2023-08-12 23:18:42 -0700

    ss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ *Description: main - Print out a string to stdout
+ *Return: 0 if sucess
+ */
 int main(void){
 puts("\"Programming is like building a multilingual puzzle");
-       
 return(0);
 }	       
