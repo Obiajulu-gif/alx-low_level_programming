a4722706632fe397a92e6563244ca42669c01666
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-08-19 21:07:02 +0100

    hello dear

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,41 @@
  */
 void print_number(int n)
 {
-  long num;
-  int is_negative;
-  int digit;
+	long m; /* power of 10 */
+	int c; /* boolean check */
+	long num; /* convert int to long */
 
-  num = n;
-  is_negative = num < 0;
+	num = n;
+	/* negatives */
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
 
-  if (is_negative)
-    _putchar('-');
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
 
-  do {
-    digit = num % 10;
-    _putchar(digit + '0');
-    num /= 10;
-  } while (num);
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
 }
-
