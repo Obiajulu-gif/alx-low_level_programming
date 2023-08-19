d9559572d1d575df0a91a1b59711e3c6ed627ad3
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-08-19 20:37:49 +0100

    hello dear

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_diaonal - function that draws a diagonal line on the terminal
+*@n: variable
+*Return: void
+*/
+void print_diagonal(int n)
+{
+    if (n <= 0)
+		_putchar('\n');
+}
