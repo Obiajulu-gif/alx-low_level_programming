0c83e3e52a04da047162f7a5a00783cd5d86d003
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-08-30 23:49:11 +0100

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,6 +12,8 @@ int _sqrt_recursion(int n)
 if (n == 0 || n == 1)
 return (n);
 return (_sqrt(0, n));
+_putchar('\n');
+
 }
 
 /**
@@ -29,4 +31,6 @@ return (-1);
 else if (n * n == x)
 return (n);
 return (_sqrt(n + 1, x));
+_putchar('\n');
+
 }
