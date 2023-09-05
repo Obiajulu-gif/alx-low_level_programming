c7a13d14a136fdda7a86dcbf740d867078468318
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-05 15:09:38 +0100

    malloc free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -66,4 +66,36 @@ while (i <= len(str))
 {
 if ((str[i] != ' ') && (str[i] != '\0'))
 {
+i++;
+}
+else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+{
+words += 1;
+i++;
+}
+else
+{
+i++;
+}
+}
+return (words);
+}
 
+/**
+* len - returns length of str
+*@str: string to be counted
+*
+* Return: length of the string
+*/
+
+int len(char *str)
+{
+int len = 0;
+
+if (str != NULL)
+{
+while (str[len])
+len++;
+}
+return (len);
+}
