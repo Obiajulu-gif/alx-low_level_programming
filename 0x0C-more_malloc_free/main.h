0c8789b05398513d5d7e305a6efb70190e1e1b0a
Obiajulu-gif <okoyeemmanuelobiajulu@gmail.com>
2023-09-08 14:38:43 +0100

    even more on malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,4 +17,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* MAIN_H 
+#endif /* MAIN_H */
