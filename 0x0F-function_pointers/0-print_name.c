19c83a6bd77349a19a45beeef97c5b27a82c21b7
Obiajulu-gif <okoyeemmanuelobiajulu@gmail.com>
2023-09-13 14:26:46 +0100

    FunctionPointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
-void print_name(char *name, void (*f)(char *)){
-    if (name == NULL || f == NULL)
-    {
-        return;
-    }
-    f(name);
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+{
+return;
+}
+f(name);
 }
