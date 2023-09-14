ec28e9314824986499fd51c131cd9c67e4e6f819
Obiajulu-gif <okoyeemmanuelobiajulu.gmail.com>
2023-09-14 05:23:08 +0100

    Struction correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,22 @@
+/* Define the struct first before assigning */
 #include "dog.h"
-
-/**
- * init_dog - inits a variable of
- *type dog
-*@d: dog identification
-*@name: name of dog
-*@age: age of dog
-*@owner: owner's name
-*/
+struct dog /**
+	* init_dog - inits a variable of
+	*type dog
+	*@d: dog identification
+	*@name: name of dog
+	*@age: age of dog
+	*@owner: owner's name
+	*/
+{
+char *name;
+float age;
+char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
-{
-(*d).name = name;
-(*d).age = age;
-(*d).owner = owner;
-}
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
