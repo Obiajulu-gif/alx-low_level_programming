52316a58e22bf44cc1d63fa3ed29cb5f3ccea70c
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-23 15:37:31 +0100

    Link List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,13 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    while (h != NULL)
-    {
-        printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        count++;
-    }
+size_t count = 0;
+while (h != NULL)
+{
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
 
-    return count;
+return (count);
 }
