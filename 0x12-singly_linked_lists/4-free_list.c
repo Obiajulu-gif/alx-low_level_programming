1cc6cfab10a2b61c1ea800740d0a30dc9a878eda
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-24 11:25:15 +0100

    Link list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,20 @@
 #include "lists.h"
+/**
+ * free_list - this free the node
+ * @head: get the head of the node
+ */
+void free_list(list_t *head)
+{
+list_t *current = head;
+list_t *temp;
 
-void free_list(list_t *head){
-    list_t *current = head;
-    list_t *temp;
+while (current != NULL)
+{
+temp = current;
+current = current->next;
 
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
+free(temp->str);
 
-        free(temp->str);
-
-        free(temp);
-    }
-    
+free(temp);
+}
 }
