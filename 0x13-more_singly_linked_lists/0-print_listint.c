dc3eea8054adbfb8a0dfd6022032ff0cb3fef5aa
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-27 01:54:22 +0100

    More on single list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - this print the number of node presence
+ * @h: get the node
+ * Return: give out the number of node
+  */
+size_t print_listint(const listint_t *h)
+{
+const listint_t *current = h;
+size_t count = 0;
+
+while (current != NULL)
+{
+current = current->next;
+count++;
+}
+return (count);
+}
