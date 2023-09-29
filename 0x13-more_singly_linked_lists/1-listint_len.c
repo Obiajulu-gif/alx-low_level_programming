3fd6d840221fc593cf1d364226b42c5aefb9c565
Obiajulu-gif <okoyeemmanuelobiajulu.gmail.com>
2023-09-30 00:18:36 +0100

    Link list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 
+/**
+ * listint_len - this print the number of node presence
+ * @h: get the node
+ * Return: give out the number of node
+ */
 size_t listint_len(const listint_t *h)
 {
-    size_t count;
-    const listint_t *current = h;
+size_t count;
+const listint_t *current = h;
 
-    count = 0;
+count = 0;
 
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
 
-    return count;
+return count;
 }
