4b6099f79f122b224226b4c3cc042c3aba631613
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-30 02:09:28 +0100

    Link list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to head of singly linked list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+listint_t *current_node;
+listint_t *next_node;
+
+current_node = head;
+while (current_node != NULL)
+{
+next_node = current_node->next;
+free(current_node);
+current_node = next_node;
+}
+
+}
