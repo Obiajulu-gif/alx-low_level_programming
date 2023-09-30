e5edab565e702c824ab122cc40620dda11cec18f
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2023-09-30 03:15:41 +0100

    Link list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,16 +5,21 @@ include "lists.h"
  * @head: pointer to the head node
  * Return: the head node's data (n)
  */
-int pop_listint(listint_t **head)
+int
+pop_listint(listint_t **head)
 {
 listint_t *temp;
 int n;
 
 if (*head == NULL)
+{
 return (0);
+}
+
 temp = *head;
 n = temp->n;
 *head = temp->next;
 free(temp);
+
 return (n);
 }
