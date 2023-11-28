e165141058c10cfa93ef5bf79469c7562fe5cf63
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-11-28 19:11:23 +0100

    task 1 done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if last_number > 5:
     print(f"{last_number} and is greater than 5")
 elif last_number == 0:
     print(f"{last_number} and is 0")
-elif last_number < 6 && last_number != 0:
+elif last_number < 6 & last_number != 0:
     print(f"{last_number} and is less than 6 and not 0")
