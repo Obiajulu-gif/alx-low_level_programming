8fb258cbc24f7b5c8a947033a724fc8be2686d4d
root <okoyeemmanuel998@gmail.com>
2023-11-27 18:30:57 -0800

    8-concat fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-<<<<<<< HEAD
 str = str[39:66] + str[106:112] + str[:6]
 print (str)
-=======
-str = str[39:]
-print(str)
->>>>>>> 5f6d6c8a8181cefb97ea4f49b2d896c1065bc465
