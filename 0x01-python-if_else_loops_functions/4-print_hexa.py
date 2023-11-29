d39b4628b45a22678f86f87d75f61fcfefc41d98
root <okoyeemmanuel998@gmail.com>
2023-11-28 22:27:42 -0800

    fix 4-print_hexa

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 for num in range(99):
     print("{} = {}".format(num, hex(num)))
 
-=======
-#!/usr/bin/python3
-for num in range(99):
-    print("{} = {}".format(num, hex(num)))
->>>>>>> 174b55ec592dbd84b53e9d6fcebe071963e6944f
