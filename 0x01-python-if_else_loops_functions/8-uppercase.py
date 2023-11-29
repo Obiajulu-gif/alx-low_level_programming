9e0f38df651c4b407f42cd811c0a208d148e63fc
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-11-29 05:45:51 +0100

    task 8 done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        result += uppercase_char
+    print(result)
