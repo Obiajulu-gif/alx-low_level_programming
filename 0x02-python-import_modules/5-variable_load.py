37b668aa2bab6dda1d43e6f5e7b8b3d79f0fc078
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-12-03 10:22:19 +0100

    task 5 done

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-import variable_load_5
+import importlib.util
+
+
+def main():
+    # Specify the file path
+    file_path = 'variable_load_5.py'
+
+    # Create a module spec
+    spec = importlib.util.spec_from_file_location('variable_load_5', file_path)
+
+    # Create the module from the spec
+    module = importlib.util.module_from_spec(spec)
+
+    # Load the module
+    spec.loader.exec_module(module)
+
+    # Access and print the variable 'a'
+    print(module.a)
+
 
 if __name__ == "__main__":
-    print(variable_load_5.a)
+    main()
