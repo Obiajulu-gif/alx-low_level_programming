f38d58736d8ab659d5942935ca8bf4b74231a474
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-12-03 04:55:43 +0100

    task 4 done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 
-
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        module_name = []
-        sorted_name = sorted(module_name)
-        if not name.startswith("__"):
-            module_name.append()
-            
-        for name in sorted_name:
-            print(name)
+    module_names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    sorted_names = sorted(module_names)
+    for name in sorted_names:
+        print(name)
