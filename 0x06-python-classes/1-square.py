285ac57c7f6645f4fcc4b75a4d6f367e40061d71
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-12-21 13:19:34 +0100

    Task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''  defines a square by: (based on 0-square.py)  '''
+
+
+class Square:
+    """
+    Args:
+        size: size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
