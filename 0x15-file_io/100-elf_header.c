70b6d76de811ddd8398cf0afee0c2e1b4dba9741
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-12-22 17:38:29 +0100

    Task 2 done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-#define BUFFER_SIZE 64
 
 void print_error(char *message)
 {
@@ -19,8 +18,8 @@ void print_elf_header(char *filename)
         print_error("Can't open file");
 
     bytes_read = read(fd, &header, sizeof(header));
-    if (bytes_read != sizeof(header))
-        print_error("Can't read ELF header");
+    if (bytes_read == -1)
+        print_error("Can't read file");
 
     if (header.e_ident[EI_MAG0] != ELFMAG0 ||
         header.e_ident[EI_MAG1] != ELFMAG1 ||
@@ -29,11 +28,73 @@ void print_elf_header(char *filename)
         print_error("Not an ELF file");
 
     printf("ELF Header:\n");
-    printf("  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++)
-        printf("%02x ", header.e_ident[i]);
-    printf("\n");
+    printf("  Magic:   %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",
+           header.e_ident[EI_MAG0], header.e_ident[EI_MAG1], header.e_ident[EI_MAG2], header.e_ident[EI_MAG3],
+           header.e_ident[EI_CLASS], header.e_ident[EI_DATA], header.e_ident[EI_VERSION], header.e_ident[EI_OSABI],
+           header.e_ident[EI_ABIVERSION], header.e_ident[EI_PAD], header.e_type, header.e_machine,
+           header.e_version, header.e_entry, header.e_phoff, header.e_shoff);
     printf("  Class:                             %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
     printf("  Data:                              %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-    printf("  Version:                           %d%s\n", header.e_ident[EI_VERSION], header.e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-    printf("  OS/ABI:                            %s\n", header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+    printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            ");
+    switch (header.e_ident[EI_OSABI])
+    {
+        case ELFOSABI_SYSV:
+            printf("UNIX - System V\n");
+            break;
+        case ELFOSABI_HPUX:
+            printf("UNIX - HP-UX\n");
+            break;
+        case ELFOSABI_NETBSD:
+            printf("UNIX - NetBSD\n");
+            break;
+        case ELFOSABI_LINUX:
+            printf("UNIX - Linux\n");
+            break;
+        case ELFOSABI_SOLARIS:
+            printf("UNIX - Solaris\n");
+            break;
+        case ELFOSABI_IRIX:
+            printf("UNIX - IRIX\n");
+            break;
+        case ELFOSABI_FREEBSD:
+            printf("UNIX - FreeBSD\n");
+            break;
+        case ELFOSABI_TRU64:
+            printf("UNIX - TRU64\n");
+            break;
+        case ELFOSABI_ARM:
+            printf("ARM\n");
+            break;
+        case ELFOSABI_STANDALONE:
+            printf("Standalone App\n");
+            break;
+        default:
+            printf("<unknown: %x>\n", header.e_ident[EI_OSABI]);
+            break;
+    }
+    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+    printf("  Type:                              ");
+    switch (header.e_type)
+    {
+        case ET_NONE:
+            printf("NONE (None)\n");
+            break;
+        case ET_REL:
+            printf("REL (Relocatable file)\n");
+            break;
+        case ET_EXEC:
+            printf("EXEC (Executable file)\n");
+            break;
+        case ET_DYN:
+            printf("DYN (Shared object file)\n");
+            break;
+        case ET_CORE:
+            printf("CORE (Core file)\n");
+            break;
+        default:
+            printf("<unknown: %x>\n", (unsigned int)header.e_type);
+            break;
+    }
+    printf("  Entry point address:               0x%lx\n", header.e_entry);
+}
