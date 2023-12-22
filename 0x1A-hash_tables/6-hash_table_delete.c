82976529c03088b333c0e82dfef683f956ff69a6
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2023-12-22 19:22:30 +0100

    Task 6 done

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ * Return: void
+ * Description: If ht is NULL, don’t do anything
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *node = NULL, *tmp = NULL;
+
+if (ht == NULL)
+{
+return;
+}
+
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+tmp = node;
+node = node->next;
+free(tmp->key);
+free(tmp->value);
+free(tmp);
+}
+}
+free(ht->array);
+free(ht);
+}
